23585b0a0252bbc9051ba7ea66dd4eb066baa1c8
Zade Dohan <105446965+zade0123@users.noreply.github.com>
2025-06-22 21:30:41 -0400

    Dist effect test 3

--- src/assets/js/hover.js
+++ src/assets/js/hover.js
@@ -7,3 +7,25 @@ new HoverEffect({
   image2: '/assets/images/whitekitchen.webp',
   displacementImage: '/assets/images/2.jpg',
 });
+
+const imageGroup1 = document.querySelector('.cs-picture1');
+if (imageGroup1) {
+  new HoverEffect({
+    parent: imageGroup1,
+    intensity: .1,
+    image1: '/assets/images/walnutkitchen.webp',
+    image2: '/assets/images/whitekitchen.webp',
+    displacementImage: '/assets/images/2.jpg',
+  });
+}
+
+const imageGroup2 = document.querySelector('.cs-picture2');
+if (imageGroup2) {
+  new HoverEffect({
+    parent: imageGroup2,
+    intensity: .1,
+    image1: '/assets/images/walnutkitchen.webp',
+    image2: '/assets/images/whitekitchen.webp',
+    displacementImage: '/assets/images/2.jpg',
+  });
+}
